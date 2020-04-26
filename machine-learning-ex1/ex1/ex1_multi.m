1668835f8affdd949ddc12169d7e6b5488a8d122
wowo_zZ <zhenggui5228@126.com>
2020-04-26 20:19:11 +0800

    ex

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -152,7 +152,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
 
-
+price = theta(1) + theta(2)*1650 + theta(3)*3
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
